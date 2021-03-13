c45b50bcd3bbf89fa766e6aee8dd1cf236faa61a
Linda Hendricks <linda.timoh@gmail>
2021-03-13 16:41:23 -0500

    commit tree a46d8d6c6b0efdb8c449406afa7b30ad839a064b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+
+function writeCards (name, event) {
+
+     let thankYouCards = []
+
+     for (let i = 0; i < name.length; i++) {
+
+     thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+
+    }
+return thankYouCards
+
+}
+
+function countDown (startingNumber) {
+    while (startingNumber > 0) {
+        console.log (startingNumber); 
+        --startingNumber;
+    }
+    console.log(startingNumber);
+}
+
+
